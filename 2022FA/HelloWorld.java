f3a02f8cba2afd357faa7a9d5dc0e21b6d3acf54
corydave <corydave@users.noreply.github.com>
2022-09-07 11:51:56 -0400

    Update HelloWorld.java

--- 2022FA/HelloWorld.java
+++ 2022FA/HelloWorld.java
@@ -1,5 +1,21 @@
+// Dave Ghidiu
+// August 29
+// This program outputs some text
+
 public class HelloWorld {
-  public static void main (String[] args) {
-    System.out.println("Hello World!");
-  }
+
+    public static void main (String[] args) {
+
+        System.out.println("Hello World!");
+        // System.out.println("DAVE!!!!");
+        // System.out.println();
+        // System.out.println("First program!!!!  😍");
+        // System.out.println();
+
+    }
+
+
+
 }
+
+
