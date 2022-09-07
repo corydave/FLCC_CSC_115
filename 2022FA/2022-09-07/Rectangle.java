5540dfb30ce499b92969076aa2a571f761d6f909
corydave <corydave@users.noreply.github.com>
2022-09-07 11:53:13 -0400

    Create Rectangle.java

---
+++ 2022FA/2022-09-07/Rectangle.java
@@ -0,0 +1,15 @@
+// Dave Ghidiu
+// 2022-09-07
+// This is a simple program to print a rectangle
+
+public class Rectangle {
+    public static void main (String[] args) {
+    
+        System.out.println("*****");
+
+        // need some more code here...
+
+        System.out.println("GAME OVER MAN!!!!");
+
+    }
+}
