e32cb5d206f53723d12600acd4f67aa14e7e5e70
corydave <corydave@users.noreply.github.com>
2022-09-19 11:32:15 -0400

    Create VariableDemo.java

---
+++ 2022FA/2022-09-19/VariableDemo.java
@@ -0,0 +1,28 @@
+public class VariableDemo {
+    public static void main (String[] args) {
+
+        // CONSTANTS are variables that cannot
+        // change value. We use SNAKE_CASE type
+        // when naming them.
+        final double SALES_TAX = 0.08;
+        // SALES_TAX = 0.075;
+
+        // camelCase is when variable names are
+        // two or more words squished together and
+        // we use capital letters to differentiate:
+        String firstName = "dave";
+        int numOfDonutsInBox = 12;
+        numOfDonutsInBox = 13;
+
+        // DECLARING a variable of type int (integer)
+        // called 'age'
+        int age;
+
+        // ASSIGNING 'age' a value of 44
+        age = 44;
+
+        // DECLARING and ASSIGNING all at once
+        double gpa = 3.76;
+
+    }
+}
