7085101d094135d2b51fbbf7485bcd29993053af
corydave <corydave@users.noreply.github.com>
2022-09-19 11:31:38 -0400

    Create MethodDemo.java

---
+++ 2022FA/2022-09-19/MethodDemo.java
@@ -0,0 +1,26 @@
+public class MethodDemo {
+    public static void main (String[] args) {
+        
+        helloWorld();
+
+        // rainyDay();
+        // sunnyDay();
+        
+        // System.out.println("\n\nGAME OVER MAN!!!\n\n");
+
+    }
+
+    public static void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void sunnyDay() {
+        System.out.println("Today is a sunny day!");
+        System.out.println("It is also warm out!");
+    }
+
+    public static void rainyDay() {
+        System.out.println("Today is a rainy day!");
+    }
+
+}
