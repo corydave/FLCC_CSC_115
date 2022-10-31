759e83dddda3b1ec1224dd76381bfe52a5412f57
corydave <corydave@users.noreply.github.com>
2022-10-31 11:35:42 -0400

    Create SwitchDemo.java

---
+++ 2022FA/2022-10-31/SwitchDemo.java
@@ -0,0 +1,18 @@
+public class SwitchDemo {
+    public static void main (String[] args) {
+
+        int choice = 20;
+
+        switch(choice) {
+
+            case 150: System.out.println("$150");
+            case 100: System.out.println("$100");
+            case 20:  System.out.println("$20");
+            case 1:   System.out.println("$1");
+
+        }
+
+        System.out.println("\nThanks.");
+
+    }
+}
